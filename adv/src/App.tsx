046937f6a1ff954f28a5762957a52459e822ce90
jesper-laptop <jesperbv4@gmail.com>
2023-02-27 12:53:49 +0100

    cleared app and index

--- adv/src/App.tsx
+++ adv/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 //Interface for components
+//Needs implementation of menu for selection 
 function App() {
   return (
     <div className="App">
--- adv/src/index.tsx
+++ adv/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//Dont change anything here
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -13,7 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+

b3613f0eae9e710f2ab3ce5ba975edad95316bc3
jesper-laptop <jesperbv4@gmail.com>
2023-02-27 13:58:55 +0100

    abstraherat array_bar komponenten

--- adv/src/components/array_bar.tsx
+++ adv/src/components/array_bar.tsx
@@ -1,13 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import { random_permutation } from "../algorithms/random_permutation";
 import Container from "./array_container";
 
-interface Props {
+type Props = {
     length: number
 }
 
 const Array_bar: React.FC<Props> = (props) => {
-    const [items, setItems] = useState<number[]>([]);
+    const array_init = random_permutation(props.length);
+    const [items, setItems] = useState<number[]>(array_init);
 
     function handleClick() {
         setItems(random_permutation(props.length));

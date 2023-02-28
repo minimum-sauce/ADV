c8f40409dcb9cc6e9b9e1594cb6066a61186887d
jesper-laptop <jesperbv4@gmail.com>
2023-02-28 12:34:40 +0100

    working input form

--- adv/src/components/sorter.tsx
+++ adv/src/components/sorter.tsx
@@ -1,29 +1,94 @@
 import React, {useEffect, useState} from "react";
+import { insertion_sort, get_frames } from "../algorithms/insertion_sort";
 import { random_permutation } from "../algorithms/random_permutation";
-import { insertion_sort } from "../algorithms/insertion_sort";
 import Array_bar from "./array_bar";
 
 interface Props {
-    algo?: (list: number[]) => number[],
-    array_init: number[]
-    frames?: number[][]
-    length?: number
+    algorithm: string
 }
 
 
 
+
+
+const initState = {value: [], current: null, ref: null};
+
+let frame_index = 0;
+var frames = [initState];
 const Sorter: React.FC<Props> = (props) => {
-    const init = props.array_init;
-    
+
+    const [items, set_items] = useState<number[]>([]);
+    const [length, set_length] = useState<number>(0);
+
+    function generate_array(event:React.FormEvent<HTMLFormElement>): void {
+        event.preventDefault()
+        set_items(random_permutation(length));
+    }
+
+    function stepBack() {
+        if(frame_index > 1) {
+            frame_index --;
+            const newFrame = frames[frame_index].value;
+            set_items(newFrame);
+        } else {frame_index = 1;};
+        
+    };
+
+    function stepForw() {
+        if(frame_index < frames.length) {
+            frame_index ++;
+            const newFrame = frames[frame_index].value;
+            set_items(newFrame);  
+        } else {frame_index = frames.length};
+        
+    }
+
+    function get_id(idx:number): string {
+        const curr = frames[frame_index].current
+        const ref = frames[frame_index].ref
+        if(idx === curr && curr !== null) {
+            return "current"
+        } else if(idx === ref && ref !== null) {
+            return "ref"
+        } else {
+            return "normal"
+        }
+    }
+
     return (
-        <div>
-            
-        <div className="sorter">
-            <Array_bar array={init}/>
-            
+        <>
+        <header className='sub-header'>
+        <form onSubmit={(e) => (generate_array(e))}>
+            <label>Number of elements:
+              <input
+                type="number" 
+                value={length}
+                onChange={(e) => (set_length(+e.target.value))}
+              />
+            </label>
+                <input 
+                    type="submit"
+                    value="Generate"
+                    className="btn btn-dark btn-block" 
+                />
+          </form>
+          
+        </header>
+        <div className="array-bar">
+            <button onClick={stepBack}>Back</button>
+            <button onClick={stepForw}>Next</button>
+            <Array_bar array={items} />
         </div>
-        </div>
+        </>
     )
 };
 
 export default Sorter
+
+
+
+
+
+
+
+

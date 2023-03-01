9cdf499c0da5bafbe9d129ca5104b25a68163a79
jesper <jesperbv4@gmail.com>
2023-03-01 14:59:18 +0100

    removed incremental_sort

--- adv/src/App.tsx
+++ adv/src/App.tsx
@@ -1,8 +1,5 @@
 import React, {useState} from 'react';
 import Sorter from './components/Sorter'
-import { insertion_sort } from './algorithms/insertion_sort';
-import { random_permutation } from './algorithms/random_permutation';
-//import Array_bar from './components/array_bar';
 import { MazeMain } from './components/Maze';
 
 import './style/App.css';
@@ -67,7 +64,6 @@ export default function App() {
       <header className='header'>
           <button onClick={()=> {set_option("insertion_sort")}}>Insertion sort</button>
           <button onClick={()=> {set_option("merge_sort")}}>Merge sort</button>
-          <button onClick={()=> {set_option("incremental_sort")}}>Incremental sort</button>
           <button onClick={()=> {set_option("dfs_maze")}}>DFS Maze</button>
         </header>
           <div className="App">

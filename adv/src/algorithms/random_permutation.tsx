14bc51fc0338056dc9ed494042c5b04edf8c7406
jesper <jesperbv4@gmail.com>
2023-03-01 13:00:55 +0100

    added function specification

--- adv/src/algorithms/random_permutation.tsx
+++ adv/src/algorithms/random_permutation.tsx
@@ -1,3 +1,8 @@
+/**
+ * Takes a number and makes a permutation 
+ * @param length number: the length for the Array
+ * @returns Array: an Array with a permutation of the numbers from 0-(number-1)
+ */
 export function random_permutation(length: number): Array<number> {
 
     function getRandomInt(min: number, max: number): number {

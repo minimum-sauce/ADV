3b241e7e1962e796bb2e43f7571c760b2042cd60
Henrik Langlott <hela4581@student.uu.se>
2023-03-03 12:05:42 +0100

    MergeSort has a working stepper

--- adv/src/algorithms/MergeSort.tsx
+++ adv/src/algorithms/MergeSort.tsx
@@ -4,51 +4,50 @@ import  '../style/MergeSort.css'
 
 type States = Array<State>;
 type State = {
-  arr_left: number[],
-  arr_right: number[],
-  position: string,
-  
+  bottom_arr: number[],
+  top_arr: number[],
+  colour?: number 
 }
 
 
-const randomArray: number[] = random_permutation(5);
-let History: States = [];
+let history: States = [];
 
-function save_state(A_left: number[], A_right: number[], position: string): void{
-  History.push(
-    {
-      arr_left: A_left,
-      arr_right: A_right,
-      position: position,
-    }
-  );
+function save_state(bottom: number[], top: number[], colour?: number): void{
+  history.push({
+    bottom_arr: bottom,
+    top_arr: top,
+    colour: colour, 
+  });
 }
+
 
 function reset_history() {
-  History = [];
-}
-
-function get_position(Current: number) {
-    return History[Current].position;
+  history = [];
 }
 
 
+function get_id(Current: number, idx: number) {
+  if(idx === history[Current].colour){
+      return 'current';
+  } else {}
+}
 
-export function merge_sort(A: Array<number>): States {
+
+export function merge_sort(A: Array<number>): void {
   reset_history();
-  save_state([...A], [], 'centre');
+  save_state([], [...A]);
   function merge_sort_helper(A: Array<number>, low: number, high: number): void {
     if (low < high) {
       const mid: number = Math.floor((low + high) / 2);
-      const A_left = A.slice(low, mid);
-      const A_right = A.slice(mid+1, high);
+      const A_left = A.slice(low, mid + 1);
+      const A_right = A.slice(mid + 1, high + 1);
       if(A_left.length > 0) { 
-        save_state(A_left, [], 'left');
+        save_state(A_left, [...A], low);
+        //split_at([mid]);
       }
       merge_sort_helper(A, low, mid);
-      save_state([A[mid + 1]], [], 'centre')
       if(A_right.length > 0) {
-        save_state([], A_right, 'right');
+        save_state(A_right, [...A], mid + 1);
       }
       merge_sort_helper(A, mid + 1, high);
       merge(A, low, mid, high);
@@ -61,48 +60,57 @@ export function merge_sort(A: Array<number>): States {
     let B_index: number = 0;
     while (left <= mid && right <= high) {
       if (A[left] <= A[right]) {
-          B[B_index] = A[left];
-          left += 1;
+        B[B_index] = A[left];
+        left += 1;
       } else {
-          B[B_index] = A[right];
-          right += 1;
+        B[B_index] = A[right];
+        right += 1;
       }
-      B_index += 1
+      save_state([...B], [...A], low);
+      B_index += 1;
     }
     while (left <= mid) {
-        B[B_index] = A[left];
-        B_index += 1;
-        left += 1;
+      B[B_index] = A[left];
+      save_state([...B], [...A], low);
+      B_index += 1;
+      left += 1;
     }
     while (right <= high) {
-        B[B_index] = A[right];
-        B_index += 1;
-        right += 1;
+      B[B_index] = A[right];
+      save_state([...B], [...A], low);
+      B_index += 1;
+      right += 1;
     }
     for (let k = 0; k < high - low + 1; k += 1) {
-        A[low + k] = B[k];
+      save_state([...B], [...A], low + k);
+      A[low + k] = B[k];
+      save_state([...B], [...A], low + k);
     }
-    save_state([...B], [], 'centre');
+    save_state([...B], [...A]);
   }
   merge_sort_helper(A, 0, A.length - 1);
-  return History;
+  save_state([], [...A]);
 };
 
-merge_sort(randomArray);
-
+let random_array: number[] = random_permutation(6);
+merge_sort(random_array);
 
 const Merge_Main: React.FC = () => {
   const [Current, setCurrent] = useState(0);
   console.log(Current);
-  
   const scrambleClick = () => {
+    random_array = random_permutation(6);
+    merge_sort(random_array);
+    setCurrent(0);
+  }
+  const unsortClick = () => {
     setCurrent(0);
   }
   const sortClick = () => {
-    setCurrent(History.length - 1);
+    setCurrent(history.length - 1);
   }
   const nextClick = () => {
-    if(Current < History.length - 1){
+    if(Current < history.length - 1){
       setCurrent(Current + 1);
     }else{}
   }
@@ -115,30 +123,27 @@ const Merge_Main: React.FC = () => {
     <div>
       <div className='buttons'>
             <button onClick={scrambleClick}>{'Scramble'}</button>
+            <button onClick={unsortClick}>{'Unsort'}</button>
             <button onClick={sortClick}>{'Sort'}</button> 
             <button onClick={backClick}>{'<'}</button>
             <button onClick={nextClick}>{'>'}</button>
         </div>
-      <div className='vertical'>
-        {History.map((His_arr, His_idx) => (
-          <div className='tree'>
-            <div className='array-bar' key={His_idx} id={get_position(His_idx)}>
-              {History[His_idx].arr_left.map((val1, idx1) => (
-                <div className='array-container' key={idx1}>
-                  {val1}
-                </div>
-               ))}
+      <div>
+        <div>Original array</div>
+        <div className='array-bar'>
+          {history[Current].top_arr.map((val, index) => (
+            <div className='array-container' key={index} id={get_id(Current, index)}>
+              {val}
             </div>
-            <div className='array-bar' key={His_idx} id={get_position(His_idx)}>
-              {History[His_idx].arr_right.map((val2, idx2) => (
-                <div className='array-container' key={idx2}>
-                  {val2}
-                </div>
-              ))}
+          ))}
+        </div>
+        <div className='array-bar'>
+          {history[Current].bottom_arr.map((val1, idx1) => (
+            <div className='array-container' key={idx1}>
+              {val1}
             </div>
-            <div id='clear'></div>
-          </div>
-        ))}
+           ))}
+        </div>
       </div>
     </div>
   )

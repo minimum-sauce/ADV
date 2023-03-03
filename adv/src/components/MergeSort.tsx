09a566fea7502607f6b201b5f48de028f6d2f7d3
jesper <jesperbv4@gmail.com>
2023-03-03 21:40:45 +0100

    added play/pause

--- adv/src/components/MergeSort.tsx
+++ adv/src/components/MergeSort.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { merge_sort } from '../algorithms/merge_sort'
 import { random_permutation } from '../algorithms/random_permutation';
+import Stepper from './Stepper'
 
 type States = Array<State>;
 type State = {
@@ -23,6 +24,25 @@ var random_array = random_permutation(6);
 merge_sort(random_array, history);
 
 const MergeMain: React.FC = () => {
+
+  // state variale to track state of play button and a function to update it 
+  const [play, set_play] = useState(false)
+  const timer = useRef<NodeJS.Timer>();
+  // play function to iterate over frames with 500ms delay
+  function handle_play() {
+    set_play(!play)
+  }
+  //Reset Interval if play is active else initiate
+  useEffect(() => {
+    if (play) {
+      timer.current = setInterval(() => next_click(), 500)
+    }
+    return () => {
+      clearInterval(timer.current)
+    }
+
+  }, [play]);
+
   const [history_idx, set_history_idx] = useState(0);
   const scramble_click = () => {
     history = [];
@@ -38,22 +58,26 @@ const MergeMain: React.FC = () => {
   }
   const next_click = () => {
     if (history_idx < history.length - 1) {
-      set_history_idx(history_idx + 1);
+      set_history_idx((x) => x + 1);
+
     } else { }
   }
+
   const back_click = () => {
     if (history_idx > 0) {
       set_history_idx(history_idx - 1);
     } else { }
   }
+
+
+
   return (
     <div>
       <div className='buttons'>
         <button onClick={scramble_click}>{'Scramble'}</button>
         <button onClick={unsort_click}>{'Unsort'}</button>
         <button onClick={sort_click}>{'Sort'}</button>
-        <button onClick={back_click}>{'<'}</button>
-        <button onClick={next_click}>{'>'}</button>
+        <Stepper prev={back_click} play={handle_play} next={next_click} />
       </div>
       <div>
         <div>Original array</div>

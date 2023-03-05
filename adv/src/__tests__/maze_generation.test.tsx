30a1ba2c70ad3e80d17eec0c883d8478cbd3c482
jesper <jesperbv4@gmail.com>
2023-03-05 20:47:02 +0100

    fixed bug see line 21

--- adv/src/__tests__/maze_generation.test.tsx
+++ adv/src/__tests__/maze_generation.test.tsx
@@ -5,7 +5,7 @@ import { generate_maze, init_grid_maze, maze_remove_wall, permutate_neighbours }
 
 describe("Maze Generation process", () => {
     const graph = graph_create_grid(3, 3);
-    
+
     test('init_grid_maze function creates a valid maze', () => {
         const reference: Maze = {
             walls: graph.node_neighbours,
@@ -16,10 +16,9 @@ describe("Maze Generation process", () => {
         }
         expect(init_grid_maze(3, 3)).toStrictEqual(reference);
     });
-    test('All nodes are explored by generate_maze function',  () => {
+    test('All nodes are explored by generate_maze function', () => {
         const maze_generation = generate_maze(4, 4);
-        const finished_maze = maze_generation[-1];
-        console.log("node_status: ", finished_maze.node_status);
+        const finished_maze = maze_generation[maze_generation.length - 1] // [-1] does not reach last index due to overloads
         finished_maze.node_status.forEach((state) => {
             expect(state).toBe(State.fully_explored);
         });
@@ -38,11 +37,11 @@ describe("Maze Generation process", () => {
     });
 
     test('All neighbours are present after permutation', () => {
-        const maze =  init_grid_maze(2, 2);
+        const maze = init_grid_maze(2, 2);
         maze.grid_graph.node_neighbours.forEach((neighbours) => {
             const permutation = permutate_neighbours(neighbours);
             permutation.forEach((value) => {
-                expect(neighbours).toContain(value); 
+                expect(neighbours).toContain(value);
             });
         });
     })
